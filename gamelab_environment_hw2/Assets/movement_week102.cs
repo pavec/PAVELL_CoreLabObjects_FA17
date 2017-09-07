a943280b2c6ce518b407fbc472af885a44076c4d
pavec <31394711+pavec@users.noreply.github.com>
2017-09-07 17:10:23 -0400

    Game Lab Homework

---
+++ gamelab_environment_hw2/Assets/movement_week102.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_week102 : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float move = Input.GetAxisRaw("Vertical") * 0.3f;
+		float rotate = Input.GetAxisRaw("Horizontal") *5;
+
+		Debug.Log (move);
+		transform.Translate (move, 0, 0);
+		transform.Rotate (rotate, 0, 0);
+
+	}
+}
