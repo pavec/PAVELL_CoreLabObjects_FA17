b495ea94ecc2773561a1495eb2277608f43f15d1
pavec <31394711+pavec@users.noreply.github.com>
2017-09-12 15:39:07 -0400

    lab homework - animation

---
+++ gamelab_rgm/Assets/scripts/carMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carMove : MonoBehaviour {
+
+
+	public float speed = 0;
+	public float force = 6;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnCollisionEnter (Collision other){
+
+		float distance = speed * Time.deltaTime;
+		transform.Translate (distance, 0, 0);
+
+	}
+}
---
+++ gamelab_rgm/Assets/scripts/falling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falling : MonoBehaviour {
+
+	float gravity = -9.8f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		 
+
+	}
+	public void ReverseGravity(){
+		gravity = -gravity;
+
+	}
+}
